e6845ace9f51eb912614015fc5c364238763b8b9
刘宇 <1243135004@qq.com>
2023-02-23 13:35:24 +0800

    核对完第16节，第十七重新核对

--- c++/main.cpp
+++ c++/main.cpp
@@ -2,15 +2,21 @@
 using namespace std;
 
 int main(){
-	int b;
-for (int i = 1; i <= 5; i++) {
-		int j = 1 * i - 1;
-		for (int a = 1; a <= j; a++) {
-			cout << b << " ";
-			b++;
-		}
-		cout << endl;
-	}
-return 0;
+	double c,h,k;
+	cout << "c=" ;	
+	cin>>c;
+	cout << "h=" ;    
+	cin>>h;
+	k=9/5*h+32;	
+	double F =9/5*c+32;
+	//c++输出
+	cout << "c++" << endl;
+	cout << "f=" << F << endl;
+	cout << "k=" << k << endl;
+	//c输出
+	printf("c\n");
+	printf("f=%.2f\n",F);
+	printf("k=%.2f\n",k);
+	//%.2f：保留两位小数, \n换行
+	return 0;
 }
-A
