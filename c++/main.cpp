774cbd8bb858f31f0f1740fc094f6244fc5aa4af
刘宇 <1243135004@qq.com>
2023-03-01 21:13:43 +0800

    更新

--- c++/main.cpp
+++ c++/main.cpp
@@ -3,11 +3,11 @@ using namespace std;
 
 int main()
 {
-    long long int  n,m=0; //使用long long int 防止用户输入数字过大
-    cin >> n ;
-    for(;n!=0;n/=10)
-        m=m*10+n%10;
-    cout << m ;
+    char buf[100];
+    printf("you name?");
+    scanf("%s", buf);
+    printf("%s\n", buf);
+    
     return 0;
 }
-3
+
