d1e94d5033d3c7d130804e81d2a43d3d3f990097
刘宇 <1243135004@qq.com>
2023-07-17 13:16:09 +0800

    跟新

--- c++/dome/ceshi/main.cpp
+++ c++/dome/ceshi/main.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
-
 using namespace std;
 
-int main()
-{
-    cout << "Hello World!" << endl;
+int main() {
+    int a = 10;     // 定义一个整数变量a
+    int* ptr = &a;  // 定义一个指向整数的指针ptr，并将a的地址赋值给ptr
+
+    cout << a << endl;    // 输出a的值，输出结果为10
+    cout << *ptr << endl; // 输出通过指针访问的值，输出结果也为10
+
     return 0;
 }
